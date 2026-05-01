4cb9922816ac9e8f331a47535a20aee1d4307478
unknown <shubhambindal2999@gmail.com>
2026-05-01 14:50:20 +0530

    files added

---
+++ first.py
@@ -0,0 +1,11 @@
+num1=11
+num2=10
+sum1=num1+num2
+print(num1,"+",num2,"=",sum1)
+print(f"{num1}") # without coma use kr skte hai f=string formating lgane k baad
+
+num1=int(input("Enter first number "))
+num2=int(input("Enter Second number "))
+result=num1+num2
+print(type)
+print(result)
